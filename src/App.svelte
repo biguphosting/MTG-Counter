3d37e0288b9f4b8de2fa1b88fc7880e9cea76a15
biguprush <rush@bigup.com.pa>
2024-08-27 19:28:23 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Tarjeta from "./Tarjeta.svelte";
+  let AzulPoints=20;
+  let RojoPoints=20;
+
+  function resetPoints(){
+    AzulPoints=20;
+    RojoPoints=20;
+  }
+</script>
+
+
+
+
+<Tarjeta name="Azul" bind:points={AzulPoints} style="primary"/>
+<Tarjeta name="Rojo" bind:points={RojoPoints} style="secondary"/>
+{#if AzulPoints<=0}
+<h3>Rojo Gana</h3>
+{:else if RojoPoints<=0}
+<h3>Azul Gana</h3>
+{:else}
+<h3>Nadie gana aún</h3>
+{/if}
+<button type="reset" on:click={resetPoints}>Reset</button>
---
+++ src/Tarjeta.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let name='';
+    export let points;
+    export let style='default';
+    function increasePoints(){
+        points++;
+    }
+
+    function decreasePoints(){
+        points--;
+    }
+ 
+</script>
+<div class="uk-card uk-card-{style} uk-width-1-6@m uk-card-small uk-margin-small uk-padding-small">
+    <div class="uk-card-header"><h3 class="uk-card-title">{name}</h3></div>
+    <div class="uk-card-body uk-heading-small uk-text-center">{points}</div>
+    <div class="uk-card-footer uk-text-center">
+        <button type="button" class="uk-button uk-button large uk-button-primary" on:click={increasePoints}>+</button>
+        <button type="button" class="uk-button uk-button large uk-button-secondary" on:click={decreasePoints}>-</button>
+    </div>
+
+</div>
